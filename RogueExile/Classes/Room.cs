1bf1dd1b1b268cb7d5b4c06425a9432f727f0adf
Fabricio Rojas <fabriciovmamanir@gmail.com>
2023-07-02 20:29:41 -0500

    updated room class

--- RogueExile/Classes/Room.cs
+++ RogueExile/Classes/Room.cs
@@ -1,6 +1,7 @@
 ﻿using RogueExile.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,11 +10,36 @@ namespace RogueExile.Classes
 {
     internal class Room : IRenderable
     {
+        public int MaxWidth = 12;
+        public int MaxHeight = 12;
+        public int MinWidth = 5;
+        public int MinHeight = 5;
+        public int Width;
+        public int Height;
+        public Cell[,] RoomGrid;
+        Random random = new();
         public Room()
         {
-
+            Width = random.Next(MinWidth, MaxWidth + 1);
+            Height = random.Next(MinHeight, MaxHeight + 1);
+            RoomGrid = new Cell[Width, Height];
         }
-
+        private void GenerateBorders()
+        {
+            for (int col = 0; col < Width; col++)
+            {
+                for (int row = 0; row < Height; row++)
+                {
+                    Cell cell = new(col, row);
+                    if (cell.X == 0 || cell.X == Width - 1 || cell.Y == 0 || cell.Y == Height - 1)
+                    {
+                        cell = cell.SetVal('█'); // https://theasciicode.com.ar/
+                        cell = cell.SetColor(ConsoleColor.Yellow);
+                    }
+                    RoomGrid[col, row] = cell;
+                }
+            }
+        }
         public void Render()
         {
 
